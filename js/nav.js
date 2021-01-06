439e5edef34defff258a03f76d9e0eccf6f76c2a
quite2333 <1186585934@qq.com>
2021-01-06 21:29:20 +0800

    add gittalk

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+$.get("./head.tpl", function(result){      
+    $("#header").html(result);
+    $("#my-slider").camRollSlider();
+});
+var timeId;
+function show(){
+  window.clearInterval(timeId);
+  $(".drop").css("display","block");
+  timeId=setTimeout(function(){$(".drop").css("display","none");},5000);
+}
